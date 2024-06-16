c16628b651a896728a316e2e6d369eeb2dfa48f7
danigggris <142274451+Danielgggris@users.noreply.github.com>
2024-06-15 22:35:14 -0300

    Arreglo interface Diccionario (prueba)

--- src/main/java/aed/Diccionario.java
+++ src/main/java/aed/Diccionario.java
@@ -22,47 +22,4 @@ interface Diccionario<K,V> {
      */
     // public String toString();
 
-    
-    
-    
-    
-    
-    public T minimo();
-
-    /**
-     * Devuelve el mayor elemento del conjunto
-     *
-     */
-    public T maximo();
-
-    /**
-     * Agrega un elemento al conjunto
-     * 
-     */
-    public void insertar(T elem);
-
-    /**
-     * Devuelve verdadero si el elemento pertenece al conjunto
-     * 
-     */
-    public boolean pertenece(T elem);
-
-    /**
-     * Elimina el elemento del donjunto
-     * 
-     */
-    public void eliminar(T elem);
-
-    /**
-     * Imprime el conjunto
-     *
-     */
-    public String toString();
-
-    /**
-     * Retorna un conjunto con los mismos elementos
-     * 
-     */
-    // public ABB<T> copiar();
-
 }
