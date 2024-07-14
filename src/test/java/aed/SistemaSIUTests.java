f848faae3ba3a207ff212ecefb4a5def0f99540a
Lucas Parma <l.parma93@gmail.com>
2024-07-14 20:36:41 -0300

    Descomente realizar inscripciones en cerrar_facultad

--- src/test/java/aed/SistemaSIUTests.java
+++ src/test/java/aed/SistemaSIUTests.java
@@ -1,8 +1,11 @@
 package aed;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SistemaSIUTests {
     String[] estudiantes;
