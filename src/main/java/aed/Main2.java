19bd2ed4becf5f47377c9ca63e14fa1773d6826e
NuviPvP <cristiantepe4589@gmail.com>
2024-07-14 16:19:40 -0300

    main 2 actualizacion

--- src/main/java/aed/Main2.java
+++ src/main/java/aed/Main2.java
@@ -1,6 +1,5 @@
 package aed;
 
-import aed.SistemaSIU.CargoDocente;
 
 public class Main2 {
 
@@ -8,30 +7,20 @@ public class Main2 {
         InfoMateria[] infoMaterias = new InfoMateria[]{
             new InfoMateria(new ParCarreraMateria[]{
                 new ParCarreraMateria("ac", "ad"),
-                //new ParCarreraMateria("acc", "add")
+                new ParCarreraMateria("acc", "add")
             }),
             new InfoMateria(new ParCarreraMateria[]{
                 new ParCarreraMateria("ac", "ad2"),
-                //new ParCarreraMateria("acc", "add")
+                new ParCarreraMateria("acc", "add2")
             }),
-            
-        };
-
-        // Crear lista de estudiantes
-        String[] estudiantes = {"12345", "67890", "11223"};
-
-        // Inicializar el sistema
+        }; 
+        String[] estudiantes = {};
         SistemaSIU sistema = new SistemaSIU(infoMaterias, estudiantes);
 
-        // Imprimir toda la información del sistema (lista de carreras, con lista materias de cada una)
+
+        System.out.println("Antes de borrar");
+
         System.out.println(sistema.toString());
-
-        /* // Inscribir estudiantes
-        sistema.inscribir("12345","Ciencias de Datos", "Algoritmos3");
-        sistema.inscribir("67890","Ciencias de la Computación", "Técnicas de Diseño de Algoritmos");
-         */
-
-        // Imprimir información de cada materia en cada carrera
         for (String nombreCarrera : sistema.carreras()) {
             Carrera carrera = sistema.getCarrera(nombreCarrera);
             for (String nombreMateria : carrera.getMaterias().claves()) {
@@ -40,13 +29,10 @@ public class Main2 {
             }
         }
 
-        sistema.cerrarMateria("ad","ac");
-
+        sistema.cerrarMateria("ad","ac"); // Tambien deberia borrar ("ad2","acc")
         System.out.println("Despues de borrar");
 
-        // Imprimir toda la información del sistema (lista de carreras, con lista materias de cada una)
         System.out.println(sistema.toString());
-
         for (String nombreCarrera : sistema.carreras()) {
             Carrera carrera = sistema.getCarrera(nombreCarrera);
             for (String nombreMateria : carrera.getMaterias().claves()) {
