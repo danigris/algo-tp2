34027c6771a5255f3c373dfe575eff189ea1010e
Lucas Parma <l.parma93@gmail.com>
2024-07-14 14:54:11 -0300

    Agregar Invariante de Representacion para Conjunto y ConjuntoDigital

--- src/main/java/aed/Conjunto.java
+++ src/main/java/aed/Conjunto.java
@@ -1,5 +1,16 @@
 package aed;
 
+/**
+ * Interfaz para un Conjunto.
+ * 
+ * Invariante de Representación:
+ * - No duplicados: Un conjunto no contiene elementos duplicados. Cada elemento en el conjunto es único.
+ * - No nulidad: Un conjunto no debe contener elementos nulos (null). Todos los elementos en el conjunto deben ser no nulos.
+ * - Tamaño no negativo: El tamaño de un conjunto debe ser un número no negativo.
+ *
+ * @param <T> el tipo de elementos en el conjunto
+ *
+ */
 public interface Conjunto<T> {
 
     boolean conjuntoVacio();
